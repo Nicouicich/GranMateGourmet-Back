05cf14e5749e17016e9cca20de8e62ef2eaa7c5e
Nicolás Uicich <nico_uicich@outlook.com>
2022-12-17 20:11:02 -0300

    commit

--- dist/index.js
+++ dist/index.js
@@ -1,1 +1,17 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Tatis = void 0;
+class Tatis {
+    constructor() {
+        this.nombre = 'luca';
+    }
+    get Nombre() {
+        return this.nombre;
+    }
+    set Nombre(nombre) {
+        this.nombre = nombre;
+    }
+}
+exports.Tatis = Tatis;
+const luca = new Tatis();
+console.log(luca.Nombre);
